fc164bde715060cf0ead07405110d89538ac0269
mandyeightysix <96144054+mandyeightysix@users.noreply.github.com>
2022-05-04 14:11:19 -0400

    Deleted console.log

--- src/index.js
+++ src/index.js
@@ -23,7 +23,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let locationElement = document.querySelector("#location");
   let descriptionElement = document.querySelector("#description");
